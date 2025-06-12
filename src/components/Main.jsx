a6b5ce1e78d964cd840c3ceb2332f0781e93e17d
miriyan19425 <miriyan19425@gmail.com>
2025-06-12 22:55:00 +0300

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import SnowmanForm from "./SnowmanForm";
+import Snowballs from "./Snowballs";
+
+function Main(){
+    return (
+        <main className="hero">
+           <SnowmanForm />
+           <Snowballs />
+        </main>
+    );
+}
+
+export default Main;
