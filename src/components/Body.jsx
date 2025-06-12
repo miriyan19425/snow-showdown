077620e246b20dd698062c3f5fdf28571770f67c
miriyan19425 <miriyan19425@gmail.com>
2025-06-12 22:54:13 +0300

    Create Body.jsx

---
+++ src/components/Body.jsx
@@ -0,0 +1,12 @@
+import Main from "./Main";
+
+function Body(){
+    return (
+        <div className="body">
+            <h1>Snow Showdown</h1>
+            <Main />
+        </div>
+    );
+}
+
+export default Body;
