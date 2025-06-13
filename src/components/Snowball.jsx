881561226e6f2357ccb8d5f3c395d750b5d5468d
miriyan19425 <miriyan19425@gmail.com>
2025-06-13 22:38:08 +0300

    Create Snowball.jsx

---
+++ src/components/Snowball.jsx
@@ -0,0 +1,10 @@
+function Snowball(){
+    return (
+        <div className="snowball">
+          <h2>&#10053;Snowman Preview&#10053;</h2>
+          <ul className="snowman-preview"></ul>
+        </div>
+    );
+}
+
+export default Snowball;
