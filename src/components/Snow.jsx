a5d0ec54135a77da8cdee8f6e90c95d0f55f3081
miriyan19425 <miriyan19425@gmail.com>
2025-06-13 22:37:09 +0300

    Create Snow.jsx

---
+++ src/components/Snow.jsx
@@ -0,0 +1,10 @@
+function Snow(){
+    return (
+        <div className="snow">
+          <h2>&#10053;Your Snowman&#10053;</h2>
+          <ul className="snow-list"></ul>
+        </div>
+    );
+}
+
+export default Snow;
