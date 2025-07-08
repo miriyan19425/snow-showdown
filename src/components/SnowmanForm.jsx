c8b3c83e3aa673347c66110ef7c711c96fa76c61
miriyan19425 <miriyan19425@gmail.com>
2025-07-08 23:21:14 +0300

    Create SnowmanForm.jsx

---
+++ src/components/SnowmanForm.jsx
@@ -0,0 +1,22 @@
+function SnowmanForm(){
+    return (
+        <div className="snowman-container">
+            <form className="snowman">
+                <input type="text" id="snowman-name" placeholder="Snowman Name" />
+                <input type="number" id="snowman-height" placeholder="Height (in centimeters)" />
+                <input type="text" id="location" placeholder="Location" />
+                <input type="text" id="creator-name" placeholder="Creator" />
+                <select id="special-attribute" placeholder="Special Attribute">
+                    <option value="" disabled selected>Special Attribute</option>
+                    <option value="Carrot Nose">Carrot Nose</option>
+                    <option value="Hat/Pot">Hat/Pot</option>
+                    <option value="Branches for Arms">Branches for Arms</option>
+                    <option value="Button Eyes">Button Eyes</option>
+                </select>
+                <button type="submit" class="add-btn">Add</button>
+            </form>
+        </div>
+    );
+}
+
+export default SnowmanForm;
