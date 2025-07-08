c93b9d1e3a311343a632299ebfe2c35d2b59b61d
miriyan19425 <miriyan19425@gmail.com>
2025-07-08 23:19:52 +0300

    Create Snowballs.jsx

---
+++ src/components/Snowballs.jsx
@@ -0,0 +1,13 @@
+import Snowball from "./Snowball";
+import Snow from "./Snow";
+
+function Snowballs(){
+    return (
+        <div class="snowballs-container">
+           <Snowball />
+           <Snow />
+        </div>
+    );
+}
+
+export default Snowballs;
