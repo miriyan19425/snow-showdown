feb069376e876beeb2e7c003b5e4b7849f0a488e
miriyan19425 <miriyan19425@gmail.com>
2025-07-12 23:39:22 +0300

    Update Snowball.jsx

--- src/components/Snowball.jsx
+++ src/components/Snowball.jsx
@@ -1,10 +1,32 @@
 import styles from './Snowball.module.css';
 
-function Snowball(){
+function Snowball({snowman}){
     return (
         <div className={styles.snowball}>
           <h2>&#10053;Snowman Preview&#10053;</h2>
-          <ul className={styles.snowmanPreview}></ul>
+          <ul className={styles.snowmanPreview}>
+            {(
+                snowman.name !== '' &&
+                snowman.height !== '' &&
+                snowman.location !== '' &&
+                snowman.creator !== '' &&
+                snowman.attribute !== undefined
+              )&&
+              <li>
+                <article>
+                  <p>Name: {snowman.name}</p>
+                  <p>Height: {snowman.height}</p>
+                  <p>Location: {snowman.location}</p>
+                  <p>Creator: {snowman.creator}</p>
+                  <p>Attribute: {snowman.attribute}</p>
+                </article>
+                <div className="btn-container">
+                  <button>Edit</button>
+                  <button>Next</button>
+                </div>
+              </li> 
+            }  
+          </ul>
         </div>
     );
 }
