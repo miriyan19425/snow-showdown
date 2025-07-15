f6344b51843fefc128fa47454c48561ebe40c526
miriyan19425 <miriyan19425@gmail.com>
2025-07-15 23:33:33 +0300

    Update Snow.jsx

--- src/components/Snow.jsx
+++ src/components/Snow.jsx
@@ -4,7 +4,21 @@ function Snow({nextSnowman, next}){
     return (
         <div className={styles.snow}>
           <h2>&#10053;Your Snowman&#10053;</h2>
-          <ul className={styles.snowList}></ul>
+          <ul className={styles.snowList}>
+              {
+              next &&
+              <li>
+                <article>
+                  <p>Name: {nextSnowman.name}</p>
+                  <p>Height: {nextSnowman.height}</p>
+                  <p>Location: {nextSnowman.location}</p>
+                  <p>Creator: {nextSnowman.creator}</p>
+                  <p>Attribute: {nextSnowman.attribute}</p>
+                  <button>Send</button>
+                </article>
+              </li>
+            }
+          </ul>
         </div>
     );
 }
