761e4dba948a066fe3b351700195ebd8760bf6c1
miriyan19425 <miriyan19425@gmail.com>
2025-07-27 20:35:55 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import Body from './components/Body'; 
+import SnowmanForm from "./components/SnowmanForm";
 
 function App() {
-   return <Body />;
+   return (
+        <div className="body">
+    <h1>Snow Showdown</h1>
+    <main id="hero">
+      <SnowmanForm />
+      <div className="snowballs-container">
+        <div className="snowball">
+          <h2>&#10053;Snowman Preview&#10053;</h2>
+          <ul className="snowman-preview"></ul>
+        </div>
+        <div className="snow">
+          <h2>&#10053;Your Snowman&#10053;</h2>
+          <ul className="snow-list"></ul>
+        </div>
+      </div>
+    </main>
+    <img id="back-img" src="./assets/back-Snowman.png" alt="" hidden />
+  </div>
+   );
 }
 
 export default App;
