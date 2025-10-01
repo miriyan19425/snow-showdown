eb7232912366609a2472c88dc41e964872e470c5
miriyan19425 <miriyan19425@gmail.com>
2025-10-01 22:30:23 +0300

    Update SnowmanPreview.jsx

--- src/components/SnowmanPreview.jsx
+++ src/components/SnowmanPreview.jsx
@@ -4,19 +4,19 @@ function SnowmanPreview({snowmanMaking}){
           <h2>&#10053;Snowman Preview&#10053;</h2>
           <ul className="snowman-preview">
             {(
-                snowman.name !== '' &&
-                snowman.height !== '' &&
-                snowman.location !== '' &&
-                snowman.creator !== '' &&
-                snowman.attribute !== undefined
+                snowmanMaking.name !== '' &&
+                snowmanMaking.height !== '' &&
+                snowmanMaking.location !== '' &&
+                snowmanMaking.creator !== '' &&
+                snowmanMaking.attribute !== undefined
               )&&
               <li>
                 <article>
-                  <p>Name: {snowman.name}</p>
-                  <p>Height: {snowman.height}</p>
-                  <p>Location: {snowman.location}</p>
-                  <p>Creator: {snowman.creator}</p>
-                  <p>Attribute: {snowman.attribute}</p>
+                  <p>Name: {snowmanMaking.name}</p>
+                  <p>Height: {snowmanMaking.height}</p>
+                  <p>Location: {snowmanMaking.location}</p>
+                  <p>Creator: {snowmanMaking.creator}</p>
+                  <p>Attribute: {snowmanMaking.attribute}</p>
                 </article>
                 <div className="btn-container">
                   <button onClick={onEdit}>Edit</button>
