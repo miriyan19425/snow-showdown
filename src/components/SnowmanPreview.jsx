a13e200045f5837dcca616de51bb6c5937369b9c
miriyan19425 <miriyan19425@gmail.com>
2025-10-08 22:20:14 +0300

    Update SnowmanPreview.jsx

--- src/components/SnowmanPreview.jsx
+++ src/components/SnowmanPreview.jsx
@@ -1,32 +1,31 @@
 function SnowmanPreview({snowmanMaking}){
     return (
         <div className="snowball">
-          <h2>&#10053;Snowman Preview&#10053;</h2>
-          
+            <h2>&#10053;Snowman Preview&#10053;</h2>
             {(
                 snowmanMaking.name !== '' &&
                 snowmanMaking.height !== '' &&
                 snowmanMaking.location !== '' &&
                 snowmanMaking.creator !== '' &&
                 snowmanMaking.attribute !== undefined
-              )||
-                <ul className="snowman-preview">
-              <li>
-                <article>
-                  <p>Name: {snowmanMaking.name}</p>
-                  <p>Height: {snowmanMaking.height}</p>
-                  <p>Location: {snowmanMaking.location}</p>
-                  <p>Creator: {snowmanMaking.creator}</p>
-                  <p>Attribute: {snowmanMaking.attribute}</p>
-                </article>
-                <div className="btn-container">
-                  <button className="edit-btn">Edit</button>
-                  <button className="next-btn">Next</button>
-                </div>
-              </li> 
-              </ul>      
+            ) 
+            ||
+            <ul className="snowman-preview">
+                <li>
+                    <article>
+                        <p>Name: {snowmanMaking.name}</p>
+                        <p>Height: {snowmanMaking.height}</p>
+                        <p>Location: {snowmanMaking.location}</p>
+                        <p>Creator: {snowmanMaking.creator}</p>
+                        <p>Attribute: {snowmanMaking.attribute}</p>
+                    </article>
+                    <div className="btn-container">
+                        <button className="edit-btn">Edit</button>
+                        <button className="next-btn">Next</button>
+                    </div>
+                </li> 
+            </ul>      
             }  
-          
         </div>
     );
 }
