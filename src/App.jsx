714c4d29ac09baf99a04769f7090b8ab5b8a2422
miriyan19425 <miriyan19425@gmail.com>
2025-10-12 19:53:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,7 @@ function App() {
             <SnowmanForm makeSnowman={getSnowmanData}/>
             <div className="snowballs-container">
                <SnowmanPreview snowmanMaking={snowmanData}/>
-               <div className="snow">
-                  <h2>&#10053;Your Snowman&#10053;</h2>
-                  <ul className="snow-list"></ul>
-              </div>
+               <SnowmanList nextSnowman={nextSnowman} next={next} />
             </div>
          </main>
          <img id="back-img" src="./assets/back-Snowman.png" alt="" hidden />
