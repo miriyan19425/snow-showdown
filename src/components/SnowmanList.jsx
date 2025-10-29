5da0ee054c35f981607216fb3c932299a89eeafe
miriyan19425 <miriyan19425@gmail.com>
2025-10-29 21:47:00 +0200

    Refactor SnowmanList component structure

--- src/components/SnowmanList.jsx
+++ src/components/SnowmanList.jsx
@@ -1,22 +1,22 @@
 function SnowmanList({nextSnowman, next}){
     return (
         <div className="snow">
-          <h2>&#10053;Your Snowman&#10053;</h2>
-          <ul className="snow-list">
-              {
-              next &&
-              <li>
-                <article>
-                  <p>Name: {nextSnowman.name}</p>
-                  <p>Height: {nextSnowman.height}</p>
-                  <p>Location: {nextSnowman.location}</p>
-                  <p>Creator: {nextSnowman.creator}</p>
-                  <p>Attribute: {nextSnowman.attribute}</p>
-                  <button className="send-btn">Send</button>
-                </article>
-              </li>
+            <h2>&#10053;Your Snowman&#10053;</h2>
+            {
+                next &&
+                <ul className="snow-list">
+                    <li>
+                        <article>
+                            <p>Name: {nextSnowman.name}</p>
+                            <p>Height: {nextSnowman.height}</p>
+                            <p>Location: {nextSnowman.location}</p>
+                            <p>Creator: {nextSnowman.creator}</p>
+                            <p>Attribute: {nextSnowman.attribute}</p>
+                            <button className="send-btn">Send</button>
+                        </article>
+                    </li>
+                </ul>
             }
-          </ul>
         </div>
     );
 }
