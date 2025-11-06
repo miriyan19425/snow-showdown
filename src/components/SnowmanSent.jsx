71703ea8ca1bec9c1a039ff42d3cab4743bc45f2
miriyan19425 <miriyan19425@gmail.com>
2025-11-06 19:11:07 +0200

    Add back button to SnowmanSent component

--- src/components/SnowmanSent.jsx
+++ src/components/SnowmanSent.jsx
@@ -2,6 +2,7 @@ function SnowmanSent(){
   return(
     <>
         <img id="back-img" src="./src/assets/back-Snowman.png" alt="" />
+        <button className="back-btn" onClick={onBack}>Back</button>
     </>
   );
 }
